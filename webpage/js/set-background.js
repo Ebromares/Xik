01b26d67885ba462deaa0f2344959848a5df5336
Ebromares Mario <122569964+KongDevs@users.noreply.github.com>
2023-03-15 02:37:33 -0600

    Actualizar set-background.js

--- webpage/js/set-background.js
+++ webpage/js/set-background.js
@@ -1,3 +1,15 @@
 var idx = Math.floor((new Date().getHours()));
 var body = document.getElementsByTagName("body")[0];
 body.className = "heaven-" + idx;
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjln98BF6YLUr39qORDhebeh0czf5GClM",
+  authDomain: "xikbd-5b9d5.firebaseapp.com",
+  projectId: "xikbd-5b9d5",
+  storageBucket: "xikbd-5b9d5.appspot.com",
+  messagingSenderId: "662258938378",
+  appId: "1:662258938378:web:4e8b0e131797ebfc42e5f3",
+  measurementId: "G-1NN1D1483D"
+};
